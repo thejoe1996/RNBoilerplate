29d55913d840a7b36b664592bfc1d53cfeba9a08
Justine Ballares <ballaresjustine@gmail.com>
2022-09-07 00:35:33 +0800

    Added custom back button

---
+++ src/navigators/NavItems.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { Actions as NavigationActions } from 'react-native-router-flux';
+import Icon from 'react-native-vector-icons/Feather';
+
+const styles = StyleSheet.create({
+  headerButton: {
+    fontSize: 24,
+    padding: 12,
+  }
+})
+
+export const backButton = () => (
+  <TouchableOpacity
+    onPress={() => NavigationActions.pop()}
+  >
+    <View style={{ alignItems: 'center' }}>
+      <Icon name='arrow-left' style={styles.headerButton} />
+    </View>
+  </TouchableOpacity>
+);
--- src/navigators/NavigationRouter.tsx
+++ src/navigators/NavigationRouter.tsx
@@ -3,12 +3,14 @@ import { Scene, Router, Stack } from 'react-native-router-flux';
 import Home from '@/src/screens/Home';
 import Login from '@/src/screens/Login';
 
+import { backButton } from './NavItems';
+
 const NavigationRouter = () => {
   return (
     <Router>
       <Stack key="root">
         <Scene key="home" title="Home" component={Home} initial />
-        <Scene key="login" title="Login" component={Login} back/>
+        <Scene key="login" title="Login" component={Login} renderLeftButton={backButton}/>
       </Stack>
     </Router>
   )
